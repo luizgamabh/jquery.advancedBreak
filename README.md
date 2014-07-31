08687447872320232cf75b50cf399f7c2b43912a
Luiz Gustavo Freire Gama <luizgamabh@gmail.com>
2014-07-31 01:47:19 -0300

    script + example + readme

---
+++ README.md
@@ -0,0 +1,32 @@
+jquery.advancedScroll
+=====================
+
+Creates new event for *window* called **break** and returns this resolution index starting from biggest to smaller.
+
+Before 1st break (biggest resolution): index 0
+Betweeen 1st and 2nd breaks: index 1
+Betweeen 2nd and 3th breaks: index 2
+Between **m**th and **n**th resolutins: index **m**
+After **n**th break (smallest resolution): index **n**
+
+## Instalation
+
+```html
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="jquery.advancedBreak.js"></script>
+<script>
+var my_breaks = [320,480,768];
+var breaks = $(window).advancedBreaks(my_breaks);
+</script>
+```
+
+## Use:
+
+```javascript
+$(window).on({
+load: function() { console.log(breaks.getIndex()); /* do something else */ },
+break: function(event, index) { console.log(index); /* do something else */ }
+});
+```
+
+Please check [jquery.advancedScrol](https://github.com/luizgamabh/jquery.advancedScroll)
---
+++ jquery.advancedBreak.js
@@ -0,0 +1,41 @@
+/*! Author: Luiz Gustavo Freire Gama - @luizgamabh */
+
+'use strict';
+
+(function($) {
+
+    $.fn.advancedBreak = function(breaks) {
+        var lastBreak = null,
+            actualBreak = null;
+        breaks = typeof breaks === 'undefined' ? null : breaks;
+        if (!(breaks instanceof Array && breaks.length > 0)) return 0;
+        breaks = breaks.sort().reverse(); //sort from bigger screen to smaller screen
+        var getBreakIndex = function(element, breaks) {
+            var innerWidth = element.innerWidth;
+            for (var i = 0, j = breaks.length; i <= j; i++) {
+                if ((innerWidth > breaks[i] && i < j) || i == j) {
+                    return i;
+                }
+            }
+        };
+        this.getIndex = function() {
+            return getBreakIndex(window, breaks);
+        };
+        return this.each(function() {
+            var _this = this,
+                $this = $(_this);
+            if (_this.self != window.self) return; // Allows only window to use this plugin
+            actualBreak = lastBreak = getBreakIndex(_this, breaks);
+            $this.resize(function() {
+                actualBreak = getBreakIndex(_this, breaks);
+                if (actualBreak != lastBreak) {
+                    lastBreak = actualBreak;
+                    $this.trigger("break", [lastBreak]);
+                }
+            });
+        });
+    };
+})(jQuery);
+
+// Uncomment the following line to enable it wherever, or call this on each page you want to use:
+// $(window).advancedBreak();
